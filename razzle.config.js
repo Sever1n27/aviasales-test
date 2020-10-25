1401f13ec932f67423076fc15e62d8f29ca1b116
Severin Gritsenko <severin@rnd.stcnet.ru>
2020-10-25 14:48:23 +0300

    added aliases and basic structure

--- razzle.config.js
+++ razzle.config.js
@@ -15,4 +15,16 @@ module.exports = {
         },
         'razzle-plugin-eslint',
     ],
+    modify: (config, { target, dev }) => {
+        config.resolve.alias = {
+            '@ui': path.resolve('src/ui/'),
+            '@core': path.resolve('src/core/'),
+            '@constants': path.resolve('src/constants/'),
+            '@pages': path.resolve('src/pages/'),
+            '@types': path.resolve('src/types/'),
+            '@features': path.resolve('src/features/'),
+            '@utils': path.resolve('src/utils/'),
+        };
+        return config;
+    },
 };
